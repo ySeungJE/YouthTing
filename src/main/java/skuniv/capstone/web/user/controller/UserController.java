d9f404371ed7222fcf217ff762cec646053e6759
ySeungJE <dbstmdwp98@naver.com>
2024-01-07 21:45:20 +0900

    240105 commit - Redis 도입으로 처음 채팅창 결 때만 DB에서 Redis로 채팅 load 하고 그 뒤엔 in-memory 로 가져오도록 함

--- src/main/java/skuniv/capstone/web/user/controller/UserController.java
+++ src/main/java/skuniv/capstone/web/user/controller/UserController.java
@@ -33,7 +33,7 @@ public class UserController {
         String storeProfileName = fileStore.storeFile(proFilePicture,userJoinDto.getName());
         userService.join(User.createUser(userJoinDto,storeProfileName));
 //            created.getSendRequestList().add(null); // 객체를 save함과 동시에 배열이 생성될 순 없는지를 확인하기 위한 문장, 일단 유저가 만들어지긴 할 거.
-        // 지렸닼ㅋㅋㅋㅋㅋ 이게 안되는 거였네... 나름대로 해석하자면 같은 트랜잭션 내에서 바로 list 속성을 사용하려면, @Builder.default 가 필수라는 거
+        // 이게 안되는 거였네... 나름대로 해석하자면 같은 트랜잭션 내에서 바로 list 속성을 사용하려면, @Builder.default 가 필수라는 거
         return User.createUser(userJoinDto,storeProfileName).getName() + "님이 가입하셨습니다";
     }
     @GetMapping("/list")
